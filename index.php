4ffafb8f5f10d348c1e68d999871d5de047ca7fb
David Herbert <dherbert@sessiondigital.com>
2014-07-28 09:27:35 +0100

    Add title and content to homepage

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
 
 $heading = 'Git Training';
 
+$content = 'This is the homepage for the Git Training course.'
+
 ?>
 <html>
+    <head>
+        <title><?php echo $heading; ?></title>
     <body>
         <h1><?php echo $heading; ?></h1>
+        <p>
+            <?php echo $content; ?>
+        </p>
     </body>
 </html>
